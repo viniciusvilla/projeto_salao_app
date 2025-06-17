6b01cc1e1daabbd2cb4442158356767f3054ed81
viniciusvilla <120747413+viniciusvilla@users.noreply.github.com>
2025-06-16 22:06:28 -0300

    Correção
    Puxei a pasta database sem querer kkk

--- app/database/database.ts
+++
@@ -1,7 +0,0 @@
-import SQLite from 'react-native-sqlite-storage';
-
-SQLite.enablePromise(true);
-
-export const getDBConnection = async () => {
-  return SQLite.openDatabase({ name: 'salao.db', location: 'default' });
-};
